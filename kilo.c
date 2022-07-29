960cd2fe56c64e7e19b0537c65d19fb869b830b4
Wasiu Bakare <wasiubakare@gmail.com>
2022-07-29 23:25:49 +0100

    Raw input and output

--- kilo.c
+++ kilo.c
@@ -1,10 +1,21 @@
 #include "terminal.h"
 
+/**
+ * die - print error message and exit
+ *
+ * @s: message to print
+ *
+ * Return: nothing
+ */
 void die(const char *s)
 {
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
 	perror(s);
 	exit(1);
 }
+
 /**
  * disableRawMode - disable terminal attributes
  *					turn on ECHO feature
@@ -12,7 +23,7 @@ void die(const char *s)
  */
 void disableRawMode(void)
 {
-	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
 		die("tcsetattr");
 }
 
@@ -23,11 +34,11 @@ void disableRawMode(void)
  */
 void enableRawMode(void)
 {
-	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+	if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1)
 		die("tcgetattr");
 	atexit(disableRawMode);
 
-	raw = orig_termios;
+	raw = E.orig_termios;
 	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
 	raw.c_oflag &= ~(OPOST);
 	raw.c_cflag |= (CS8);
@@ -39,6 +50,108 @@ void enableRawMode(void)
 		die("tcsetattr");
 }
 
+char editorReadKey(void)
+{
+	int nread;
+	char c;
+
+	while ((nread = read(STDIN_FILENO, &c, 1)) != 1)
+	{
+		if (nread == -1 && errno != EAGAIN)
+			die("read");
+	}
+	return (c);
+}
+
+void editorProcessKeypress(void)
+{
+	char c = editorReadKey();
+
+	switch (c)
+	{
+	case CTRL_KEY('q'):
+		write(STDOUT_FILENO, "\x1b[2J", 4);
+		write(STDOUT_FILENO, "\x1b[H", 3);
+		exit(0);
+		break;
+	}
+}
+
+int getWindowSize(int *rows, int *cols)
+{
+	struct winsize ws;
+
+	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+	{
+		if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
+			return (-1);
+		return getCursorPosition(rows, cols);
+	}
+	else
+	{
+		*cols = ws.ws_col;
+		*rows = ws.ws_row;
+		return (0);
+	}
+}
+
+int getCursorPosition(int *rows, int *cols)
+{
+	char buf[32];
+	unsigned int i = 0;
+
+	if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+		return (-1);
+
+	while (i < sizeof(buf) - 1)
+	{
+		if (read(STDIN_FILENO, &buf[i], 1) != 1)
+			break;
+		if (buf[i] == 'R')
+			break;
+		i++;
+	}
+	buf[i] = '\0';
+
+	if (buf[0] != '\x1b' || buf[1] != '[')
+		return (-1);
+	if (sscanf(&buf[2], "%d;%d", rows, cols) != 2)
+		return (-1);
+
+	return (0);
+}
+
+void editorDrawRows(void)
+{
+	int y;
+
+	for (y = 0; y < E.screenrows; y++)
+	{
+		write(STDOUT_FILENO, "~", 1);
+
+		if (y == E.screenrows - 1)
+		{
+			write(STDOUT_FILENO, "\r\n", 2);
+		}
+	}
+}
+
+void editorRefreshScreen(void)
+{
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
+	editorDrawRows();
+
+	write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+void initEditor(void)
+{
+	if (getWindowSize(&E.screenrows, &E.screencols) == -1)
+		die("getWindowSize");
+}
+
 /**
  * main - entry point to the program
  *
@@ -47,21 +160,12 @@ void enableRawMode(void)
 int main(void)
 {
 	enableRawMode();
+	initEditor();
 
 	while (1)
 	{
-		char c = '\0';
-		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
-			die("read");
-		read(STDIN_FILENO, &c, 1);
-
-		if (iscntrl(c))
-			printf("%d\r\n", c);
-		else
-			printf("%d ('%c')\r\n", c, c);
-
-		if (c == 'q')
-			break;
+		editorRefreshScreen();
+		editorProcessKeypress();
 	}
 
 	return (0);
--- terminal.h
+++ terminal.h
@@ -7,12 +7,36 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <errno.h>
+#include <sys/ioctl.h>
 
-struct termios orig_termios;
+#define CTRL_KEY(k) ((k)&0x1f)
+
+/* data */
+typedef struct editorConfig
+{
+	int screenrows;
+	int screencols;
+	struct termios orig_termios;
+} e_conf;
+e_conf E;
 struct termios raw;
 
+/* terminal */
 void disableRawMode(void);
 void enableRawMode(void);
 void die(const char *s);
+char editorReadKey(void);
+int getWindowSize(int *rows, int *cols);
+int getCursorPosition(int *rows, int *cols);
+
+/* input */
+void editorProcessKeypress(void);
+
+/* output */
+void editorRefreshScreen(void);
+void editorDrawRows(void);
+
+/* init */
+void initEditor(void);
 
 #endif /* _TERMINAL_H_ */
